b1b8b88f921f924a361edfcd7a4f64f70fac571e
nobodyoriginal <78735390+nobodyoriginal@users.noreply.github.com>
2022-09-20 14:03:47 +0300

    Delete src directory

--- src/main/java/net/ND1712/techmod/blocks/Blockstates.java
+++
@@ -1,7 +0,0 @@
-package net.ND1712.techmod.blocks;
-
-import net.minecraft.world.level.block.state.properties.IntegerProperty;
-
-public class Blockstates {
-	public static final IntegerProperty CHARGE = IntegerProperty.create("charge", 0, 4);
-}
